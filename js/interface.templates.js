8a799d34cd3dab83ef269fc2e0cb4c1c4d74a2be
Ricardo Seromenho <ricardo@seromenho.com>
2017-11-17 16:16:55 +0000

    item to show html

--- js/interface.templates.js
+++ js/interface.templates.js
@@ -7,31 +7,31 @@ this["Fliplet"]["Widget"]["Templates"]["templates.list"] = Handlebars.template({
 
   return "    <h4>"
     + alias2(((helper = (helper = helpers.key || (data && data.key)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(alias1,{"name":"key","hash":{},"data":data}) : helper)))
-    + "</h4>\r\n    <div class=\"panel-Item workflow-list-wrapper\" id=\"workflow_"
+    + "</h4>\n    <div class=\"panel-Item workflow-list-wrapper\" id=\"workflow_"
     + alias2(container.lambda((depth0 != null ? depth0.id : depth0), depth0))
-    + "\" role=\"tablist\" aria-multiselectable=\"true\">\r\n"
+    + "\" role=\"tablist\" aria-multiselectable=\"true\">\n"
     + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.items : depth0),{"name":"each","hash":{},"fn":container.program(2, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "    </div>\r\n    <p>\r\n      <button class=\"btn btn-Add\" type=\"submit\">Edit</button>\r\n    </p>\r\n";
+    + "    </div>\n    <p>\n      <button class=\"btn btn-Add\" type=\"submit\">Edit</button>\n    </p>\n";
 },"2":function(container,depth0,helpers,partials,data) {
-    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+    var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "        <div class=\"panel panel-Item\">\r\n          <div role=\"tab\" id=\"headingItem"
+  return "        <div class=\"panel panel-Item\">\n          <div role=\"tab\" id=\"headingItem"
     + alias4(((helper = (helper = helpers.ID || (depth0 != null ? depth0.ID : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"ID","hash":{},"data":data}) : helper)))
-    + "\">\r\n            <h5 class=\"panel-Item heading panel-Itemtitle collapsed\" role=\"button\" data-toggle=\"collapse\" href=\"#collapseItem"
+    + "\">\n            <h5 class=\"panel-Item heading panel-Itemtitle collapsed\" role=\"button\" data-toggle=\"collapse\" href=\"#collapseItem"
     + alias4(((helper = (helper = helpers.ID || (depth0 != null ? depth0.ID : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"ID","hash":{},"data":data}) : helper)))
-    + "\">\r\n              "
+    + "\">\n              "
     + alias4(((helper = (helper = helpers["Item Title"] || (depth0 != null ? depth0["Item Title"] : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"Item Title","hash":{},"data":data}) : helper)))
-    + "\r\n            </h5>\r\n          </div>\r\n\r\n          <div id=\"collapseItem"
+    + "\n            </h5>\n          </div>\n\n          <div id=\"collapseItem"
     + alias4(((helper = (helper = helpers.ID || (depth0 != null ? depth0.ID : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"ID","hash":{},"data":data}) : helper)))
     + "\" class=\"panel-collapse collapse\" role=\"tabpanel\" aria-labelledby=\"headingItem"
     + alias4(((helper = (helper = helpers.ID || (depth0 != null ? depth0.ID : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"ID","hash":{},"data":data}) : helper)))
-    + "\">\r\n            <div class=\"panel-body animated fadeIn\" style=\"color:#555;border-top:0px\">\r\n              <p> "
-    + alias4(((helper = (helper = helpers["Item Description"] || (depth0 != null ? depth0["Item Description"] : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"Item Description","hash":{},"data":data}) : helper)))
-    + " </p>\r\n            </div>\r\n          </div>\r\n        </div>\r\n";
+    + "\">\n            <div class=\"panel-body animated fadeIn\" style=\"color:#555;border-top:0px\">\n              <p> "
+    + ((stack1 = ((helper = (helper = helpers["Item Description"] || (depth0 != null ? depth0["Item Description"] : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"Item Description","hash":{},"data":data}) : helper))) != null ? stack1 : "")
+    + " </p>\n            </div>\n          </div>\n        </div>\n";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1;
 
-  return "<div class=\"workflow-list\">\r\n\r\n"
+  return "<div class=\"workflow-list\">\n\n"
     + ((stack1 = helpers.each.call(depth0 != null ? depth0 : (container.nullContext || {}),depth0,{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
     + "</div>";
 },"useData":true});
